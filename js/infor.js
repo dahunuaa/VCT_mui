76fcca408d603fbfb248ae5e04d860051a091b88
dahu yao <dahu yao>
2017-08-12 22:24:49 +0800

    first commit

---
+++ js/infor.js
@@ -0,0 +1,53 @@
+var inforData = [{
+	value: '100',
+	text: '技术',
+	children: [{
+		value: "101",
+		text: "1-2-1"
+	}, {
+		value: "102",
+		text: "LessonLearn"
+	}, {
+		value: "103",
+		text: "操作工培训资料"
+	}]
+},{
+	value: '200',
+	text: '质量',
+	children: [{
+		value: "201",
+		text: "质量说明"
+	}, {
+		value: "202",
+		text: "缺陷目录"
+	}, {
+		value: "203",
+		text: "客户投诉"
+	}]
+},{
+	value: '300',
+	text: '生产',
+	children: [{
+		value: "301",
+		text: "管理规则"
+	}, {
+		value: "302",
+		text: "荣誉墙"
+	}, {
+		value: "303",
+		text: "临时通知"
+	}]
+},{
+	value: '400',
+	text: '服务部',
+	children: [{
+		value: "401",
+		text: "机修"
+	}, {
+		value: "402",
+		text: "工装"
+	}, {
+		value: "403",
+		text: "物流"
+	}]
+}]
